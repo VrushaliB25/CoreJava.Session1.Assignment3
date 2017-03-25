a2c630692e3004704734a7fbeedd4bfb2871ef5e
VrushaliB25 <vrbhoite@gmail.com>
2017-03-25 22:45:53 +0530

    Core Java Session 1 Assignment 3
    Code

---
+++ AssignThree/src/AssignThree.java
@@ -0,0 +1,17 @@
+
+public class AssignThree {
+	public static void main(String s[])
+	{
+		//To Write a program that swaps the values of two variables without using third variable.
+		int a=50;
+		int b=70;
+		System.out.println("Before swaping the value of a is  "+a+" and the value of b is "+b);
+				a=a+b;
+				b=a-b;
+				a=a-b;
+				System.out.println("After swaping the value of a is  "+a+" and the value of b is "+b);
+				
+		
+	}
+
+}
